c7e1ec185fc16bc3b0f4696106b94f441b597efd
Vinh Trung <Vinh Trung@DESKTOP-B7DTBS7>
2026-09-09 16:44:01 +0700

    Add project files.

---
+++ Ecommerce.Web/Ecommerce.Core/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
